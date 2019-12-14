6f9706a827b1a1559753788992c6fa25ad5f6e6b
Your Name <you@example.com>
2019-12-13 23:30:29 -0500

    Se agregan componentes ProductList y ProductAlerts

---
+++ src/app/products.ts
@@ -0,0 +1,58 @@
+export const products = [
+    {
+      name: 'Abrigos y Chaquetas',
+      price: 799,
+      description: 'Abrigos y Chaquetas para la industria'
+    },
+    {
+      name: 'Chalecos',
+      price: 699,
+      description: 'Chalecos para la industria'
+    },
+    {
+      name: 'Conjuntos',
+      price: 299,
+      description: 'Conjuntos para la industria'
+    },
+    {
+    name: 'Delantales',
+    price: 799,
+    description: 'Delantales para la industria'
+    },
+    {
+    name: 'Escafandras',
+    price: 699,
+    description: 'Escafandras para la industria'
+    },
+    {
+    name: 'Fontaneros',
+    price: 299,
+    description: 'Fontaneros para la industria'
+    },
+    {
+        name: 'Gabardinas y Capas',
+        price: 799,
+        description: 'Gabardinas y Capas para la industria'
+      },
+      {
+        name: 'Pantalones',
+        price: 699,
+        description: 'Pantalones para la industria'
+      },
+      {
+        name: 'Prendas Cuarto Frío',
+        price: 299,
+        description: 'Prendas Cuarto Frío para la industria'
+      },
+      {
+      name: 'Ropa Dotación',
+      price: 799,
+      description: 'Ropa Dotación para la industria'
+      },
+      {
+      name: 'Señalización',
+      price: 699,
+      description: 'Señalización para la industria'
+      }
+  ];
+  
